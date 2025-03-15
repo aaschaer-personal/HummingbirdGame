bde1815ddaa6a85f2b0778eeb72c183e30b64787
aaschaer-personal <aaschaer@gmail.com>
2025-03-15 11:08:44 -0500

    rework font

--- src/UI/credits.gd
+++ src/UI/credits.gd
@@ -5,5 +5,9 @@ extends NinePatchRect
 func _ready():
 	exit_button.pressed.connect(close)
 	
+func _input(event):
+	if event.is_action_released("Esc"):
+		close()
+	
 func close():
 	visible = false
